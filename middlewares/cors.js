d541fd89b9e47f9a404097e25df3b64b88c973b0
Maor-Gross <maorgross247@gmail.com>
2025-07-09 20:53:30 +0300

    update-cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,6 +5,7 @@ const corsmiddleware = cors({
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://www.cardsproject.co.il",
+    "https://full-stack-z4c4.onrender.com"
   ],
 });
 
